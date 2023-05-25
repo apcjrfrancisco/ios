9bb473be58fa72bf3965b398941a798a3c749dc7
JanTerenceFrancisco <franciscoterence98@gmail.com>
2023-05-26 00:32:41 +0800

    Dashboard Update TMIOS

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
 
     public function index()
     {
-        return view('admin.dashboard');
+        $orders = Order::orderBy('id', 'desc')->get();
+        $orderComplete = Order::where('status_message', 'completed')->get();
+        $orderCompleteCount = Order::where('status_message', 'completed')->count();
+        $orderCount = Order::count();
+        return view('admin.dashboard', compact('orders', 'orderComplete', 'orderCompleteCount', 'orderCount'));
     }
-
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,34 +1,143 @@
 @extends('layouts.admin')
 @section('admin')
+    <div class="row">
+        <div class="col-md-12 grid-margin">
+            <div class="d-flex justify-content-between flex-wrap">
+                <div class="d-flex align-items-end flex-wrap">
+                    <div class="me-md-3 me-xl-5">
+                        <h2>Welcome back, {{ Auth::user()->name }}</h2>
+                    </div>
+                    <div class="d-flex">
+                    </div>
+                </div>
 
-<div class="row">
-    <div class="col-md-12 grid-margin">
-      <div class="d-flex justify-content-between flex-wrap">
-        <div class="d-flex align-items-end flex-wrap">
-          <div class="me-md-3 me-xl-5">
-            <h2>Welcome back, {{ Auth::user()->name }}</h2>
-            <p class="mb-md-0">Your analytics dashboard template.</p>
-          </div>
-          <div class="d-flex">
-            <i class="mdi mdi-home text-muted hover-cursor"></i>
-            <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
-            <p class="text-primary mb-0 hover-cursor">Analytics</p>
-          </div>
+            </div>
         </div>
-        <div class="d-flex justify-content-between align-items-end flex-wrap">
-          <button type="button" class="btn btn-light bg-white btn-icon me-3 d-none d-md-block ">
-            <i class="mdi mdi-download text-muted"></i>
-          </button>
-          <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
-            <i class="mdi mdi-clock-outline text-muted"></i>
-          </button>
-          <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
-            <i class="mdi mdi-plus text-muted"></i>
-          </button>
-          <button class="btn btn-primary mt-2 mt-xl-0">Generate report</button>
-        </div>
-      </div>
     </div>
-  </div>
-    
+
+    <div class="row">
+        <div class="col-md-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body dashboard-tabs p-0">
+                    <ul class="nav nav-tabs px-4" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="overview-tab" data-bs-toggle="tab" href="#overview"
+                                role="tab" aria-controls="overview" aria-selected="true">Overview</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content py-0 px-0">
+                        <div class="tab-pane fade show active" id="overview" role="tabpanel"
+                            aria-labelledby="overview-tab">
+                            <div class="d-flex flex-wrap justify-content-xl-between">
+                                <div
+                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                                    <i class="mdi mdi-currency-usd me-3 icon-lg text-danger"></i>
+                                    <div class="d-flex flex-column justify-content-around">
+                                        <small class="mb-1 text-muted">Total Sales</small>
+                                        @php
+                                            $total_amount = 0;
+                                            $revenue = 0;
+                                        @endphp
+                                        @forelse ($orderComplete as $item)
+                                            @php
+                                                $orderItem = App\Models\OrderItem::where('order_id', $item->id)->sum('total_price');
+                                                $orderItemId = App\Models\OrderItem::where('order_id', $item->id)->first();
+                                                $purchase = App\Models\Purchase::where('status', '1')->sum('buying_price');
+                                                $total_amount += $orderItemId->quantity * $orderItemId->price;
+                                                $revenue = $total_amount - $purchase;
+                                            @endphp
+                                        @empty
+                                        @endforelse
+                                        <h5 class="me-2 mb-0">
+                                            <span style="font-family: DejaVu Sans; sans-serif;">&#8369;</span>
+                                            {{ $total_amount }}
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div
+                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                                    <i class="mdi mdi-eye me-3 icon-lg text-success"></i>
+                                    <div class="d-flex flex-column justify-content-around">
+                                        <small class="mb-1 text-muted">Revenue</small>
+                                        <h5 class="me-2 mb-0"><span
+                                                style="font-family: DejaVu Sans; sans-serif;">&#8369;</span>
+                                            {{ $revenue }}</h5>
+                                    </div>
+                                </div>
+                                <div
+                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                                    <i class="mdi mdi-download me-3 icon-lg text-warning"></i>
+                                    <div class="d-flex flex-column justify-content-around">
+                                        <small class="mb-1 text-muted">Orders Completed</small>
+                                        <h5 class="me-2 mb-0">{{ $orderCompleteCount }}</h5>
+                                    </div>
+                                </div>
+                                <div
+                                    class="d-flex py-3 border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                                    <i class="mdi mdi-flag me-3 icon-lg text-danger"></i>
+                                    <div class="d-flex flex-column justify-content-around">
+                                        <small class="mb-1 text-muted">Total Orders</small>
+                                        <h5 class="me-2 mb-0">{{ $orderCount }}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4>Recent Orders</h4>
+                    <br>
+                    <table class="table table-bordered table-striped dt-responsive nowrap"
+                        style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>Tracking No.</th>
+                                <th>Name</th>
+                                <th>Payment Mode</th>
+                                <th>Ordered Date</th>
+                                <th>Amount</th>
+                                <th>Status Message</th>
+                                <th width="20%">Action</th>
+
+                        </thead>
+
+                        <tbody>
+
+
+
+                            @forelse ($orders as $item)
+                                @php
+                                    $orderItem = App\Models\OrderItem::where('order_id', $item->id)->sum('total_price');
+                                @endphp
+                                <tr class="text-center">
+                                    <td> {{ $item->tracking_no }} </td>
+                                    <td> {{ $item->name }} </td>
+                                    <td> {{ $item->payment_mode }} </td>
+                                    <td> {{ $item->created_at->format('m/d/Y') }} </td>
+                                    <td><span style="font-family: DejaVu Sans; sans-serif;">&#8369;</span>
+                                        {{ $orderItem }} </td>
+                                    <td> {{ $item->status_message }} </td>
+                                    <td><a href="{{ url('admin/orders/' . $item->id) }}"
+                                            class="btn btn-primary btn-sm">View</a></td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="8">No Orders Available</td>
+                                </tr>
+                            @endforelse
+
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
 @endsection
